e8606f7dd88316b0932e097043aa86320b798694
tbwoody <botangollo@gmail.com>
2016-12-08 15:34:30 -0800

    Update pr.js

--- pr/bob/pr.js
+++ pr/bob/pr.js
@@ -79,6 +79,55 @@ function buildPaymentRequest() {
 
 var request = buildPaymentRequest();
 
+
+// When user selects a shipping address
+request.addEventListener('shippingaddresschange', e => {
+  e.updateWith(((details, addr) => {
+    var shippingOption = {
+      id: '',
+      label: '',
+      amount: { currency: 'USD', value: '0.00' },
+      selected: true
+    };
+    // Shipping to US is supported
+    if (addr.country === 'US') {
+      shippingOption.id = 'us';
+      shippingOption.label = 'Standard shipping in US';
+      shippingOption.amount.value = '0.00';
+      details.total.amount.value = '55.00';
+    // Shipping to JP is supported
+    } else if (addr.country === 'JP') {
+      shippingOption.id = 'jp';
+      shippingOption.label = 'International shipping';
+      shippingOption.amount.value = '10.00';
+      details.total.amount.value = '65.00';
+    // Shipping to elsewhere is unsupported
+    } else {
+      // Empty array indicates rejection of the address
+      details.shippingOptions = [];
+      return Promise.resolve(details);
+    }
+    // Hardcode for simplicity
+    if (details.displayItems.length === 2) {
+      details.displayItems[2] = shippingOption;
+    } else {
+      details.displayItems.push(shippingOption);
+    }
+    details.shippingOptions = [shippingOption];
+
+    return Promise.resolve(details);
+  })(details, request.shippingAddress));
+});
+
+// When user selects a shipping option
+request.addEventListener('shippingoptionchange', e => {
+  e.updateWith(((details) => {
+    // There should be only one option. Do nothing.
+    return Promise.resolve(details);
+  })(details));
+});
+
+
 /**
  * Launches payment request for Bob Pay.
  */
