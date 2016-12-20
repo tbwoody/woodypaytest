58351564dcce327b6c4bf7f0b5c133a778a214de
tbwoody <botangollo@gmail.com>
2016-12-20 14:52:15 -0800

    Update pr.js

--- pr/bob/pr.js
+++ pr/bob/pr.js
@@ -198,8 +198,8 @@ function buildPaymentRequest() {
         isRecurring: false,
         merchantName: 'ABC Store',
         billingAddressRequired: false,
-        allowedCardBrand:['VI','DS']
-        //allowedCardBrand:['VI','MC','AX','DS']
+        //allowedCardBrand:['VI','DS']
+        allowedCardBrand:['VI','MC','AX','DS']
       }
     }
   ];
