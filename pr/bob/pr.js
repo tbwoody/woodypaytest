8c0411e0bbaa08ab61d7b823d90c22805df1c2ad
tbwoody <botangollo@gmail.com>
2017-01-10 17:12:33 -0800

    Update pr.js

--- pr/bob/pr.js
+++ pr/bob/pr.js
@@ -422,6 +422,22 @@ function onBuyClicked() {  // eslint-disable-line no-unused-vars
   }
 }
 
+function ValidURL(str) {
+  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+    '(\#[-a-z\d_]*)?$','i'); // fragment locater
+  if(!pattern.test(str)) {
+    alert("Please enter a valid URL.");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+
 function onAddMethodClicked() {
   var ul = document.getElementById("supportedMethodsList");
   var li = document.createElement("li");
@@ -430,6 +446,10 @@ function onAddMethodClicked() {
   if(methodName==null || methodName.trim()==""){
     error('Pleae enter the payment method!');
     return;
+  }
+  
+  if(ValidURL(methodName)){
+    error('This is not a valid url!');
   }
   
   li.appendChild(document.createTextNode(methodName));
