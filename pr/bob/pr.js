4e63194db026f7314c0f4fc822ee99b8c31e4867
tbwoody <botangollo@gmail.com>
2017-09-22 15:15:12 -0700

    Update pr.js

--- pr/bob/pr.js
+++ pr/bob/pr.js
@@ -408,7 +408,7 @@ function onBuyClicked() {  // eslint-disable-line no-unused-vars
     return;
   }
   
-
+/*
   try {
     request.show()
         .then(function(instrumentResponse) {
@@ -432,6 +432,29 @@ function onBuyClicked() {  // eslint-disable-line no-unused-vars
     //request = buildPaymentRequest();
   }
 }
+*/
+	
+
+request.show()
+.then(function(success){
+console.log("Chrome happy flow is done!");
+console.log(success);
+//checkoutInfoResp = document.createTextNode(success);
+//makePaymentArea.appendChild(checkoutInfoResp);
+request.complete('success')
+.then(function() {
+    done("thank you", success);
+})
+.catch(function(err) {
+    alert('error in makpayment ' + err);
+});
+})
+.catch(function(error) {
+//checkoutInfoResp = document.createTextNode(error);
+//makePaymentArea.appendChild(checkoutInfoResp);
+});
+}	
+	
 
 function ValidURL(str) {
   var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
